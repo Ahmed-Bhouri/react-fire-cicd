e6d3213eabc0636b3f21dfba37b9fd2e209605d5
Ahmed Bhouri <49922043+Ahmed-Bhouri@users.noreply.github.com>
2024-08-06 16:10:10 +0100

    ⚙️ Added CI/CD

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ function App() {
         <p>
           Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link">
+          We Added CI/CD!
         </a>
       </header>
     </div>
