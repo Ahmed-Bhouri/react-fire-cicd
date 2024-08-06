4cc66210e47788225123d70336c79c7598e7eebd
Ahmed Bhouri <49922043+Ahmed-Bhouri@users.noreply.github.com>
2024-08-06 17:15:41 +0100

    🧪 Added Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('renders Hello World! text', () => {
+    const textElement = screen.getByText(/Hello World!/i);
+    expect(textElement).toBeInTheDocument();
+  })
+
+  it('renders CI/CD text', () => {
+    const textElement = screen.getByTestId("cicd");
+    expect(textElement).toBeInTheDocument();
+    expect(textElement).toHaveTextContent("We Added CI/CD!");
+    expect(textElement).toHaveClass("App-link");
+  })
+
+  it('renders New Feature text', () => {
+    const textElement = screen.getByTestId("nf");
+    expect(textElement).toBeInTheDocument();
+    expect(textElement).toHaveTextContent("Added New Feature ⭐!");
+    expect(textElement).toHaveClass("App-link");
+  })
+
 });
